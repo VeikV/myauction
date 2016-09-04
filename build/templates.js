5ed61f272074b4a71a56f371ed26f1802bdd8349
Nikkking <veroandnika@gmail.com>
2016-09-04 17:44:16 +0300

    got data from json

--- build/templates.js
+++ build/templates.js
@@ -1,10 +1,16 @@
 Handlebars.registerPartial("product", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
+    return "<div class=\"product\">\r\n  <div class=\"product__img\">\r\n    <div class=\"product__img-link\"><img src=\"\" alt=\"There was an item image\"></div>\r\n  </div>\r\n\r\n  <div class=\"product__label\">Label</div>\r\n\r\n  <div class=\"product__title\">\r\n    <div class=\"product__title-link\">Title</div>\r\n  </div>\r\n\r\n  <div class=\"product__category\">\r\n    <div class=\"product__category-link\">Shoes</div>\r\n  </div>\r\n\r\n  <div classs=\"product__add-to-cart\">\r\n    <a class product__add-to-cart-link>Add to cart</a>\r\n  </div>\r\n\r\n\r\n\r\n <div class=\"product__price\">2$</div>\r\n  </div>\r\n</div>";
 },"useData":true}));
 this["Myauction"] = this["Myauction"] || {};
 this["Myauction"]["_product"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
+    return "<div class=\"product\">\r\n  <div class=\"product__img\">\r\n    <div class=\"product__img-link\"><img src=\"\" alt=\"There was an item image\"></div>\r\n  </div>\r\n\r\n  <div class=\"product__label\">Label</div>\r\n\r\n  <div class=\"product__title\">\r\n    <div class=\"product__title-link\">Title</div>\r\n  </div>\r\n\r\n  <div class=\"product__category\">\r\n    <div class=\"product__category-link\">Shoes</div>\r\n  </div>\r\n\r\n  <div classs=\"product__add-to-cart\">\r\n    <a class product__add-to-cart-link>Add to cart</a>\r\n  </div>\r\n\r\n\r\n\r\n <div class=\"product__price\">2$</div>\r\n  </div>\r\n</div>";
 },"useData":true});
-this["Myauction"]["product-list"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
-},"useData":true});
+this["Myauction"]["product-list"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = container.invokePartial(partials.product,depth0,{"name":"product","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"usePartial":true,"useData":true});
--- src/blocks/product-list/product-list.js
+++ src/blocks/product-list/product-list.js
@@ -1,33 +1,61 @@
 var App = App || {};//создаем глобальную переменную. Пространство имен приложения.
 
-App.instances = App.instances || {};//создаем св-во объекта
+App.instances = App.instances || {};//создаем св-во объекта, будет хранить все инстансы. Инстансы - это объекты созданные при вызове ф-ции кнструктора со словом new
 
-App.classes = App.classes || {};//создаем второе св-во объекта
+App.classes = App.classes || {};//создаем второе св-во объекта. Будет хранить все классы(функции конструткоры и их прототипы), которые создают instances
 
 //в App.classes будут храниться все классы, те классы это функции конструкторы и их прототипы
 
-App.classes.ProductList = function(element) {
-	var $root = $(element);
-
-	this.data = null;
-	this.elements = {
-		$root: $root
+App.classes.ProductList = function(element) { //описываем ф-цию конструткор. Элемент каждый раз будет ссылаться на аргумент(переданное занчение при вызове ф-ции), переданный при создании конкретного instance
+	var $root = $(element);//создаем jquery объект и кладем его в переменную, на основе element для каждого instance 
+	//console.log(element);
+	//console.log($root);
+	//this ссылка на instance(объект созданный на основе функции конструткор)
+	this.data = null; //this.data это св-во instance в которое мы будем класть данные, полученные с сервера
+	this.elements = { //в этом св-ве мы будем хранить все элементы, с которыми мы будем работать в рамках рута
+		$root: $root //в св-во объекта this.elements мы записывает значение переменной $root, далее мы сможем обращаться к этой переменной через this.elements.$root
 	};
 	//console.log(this.elements.$root);
 	this.init();//есть цепочка прототипов, и если этот метод не существует в объекте, он берется из прототипа, и так ниже.
+//instance создается в цикле в самовызывающейся ф-ции(описано внизу стр-цы)
+};
+//метод init используется для того, чтобы вызывать другие методы 
+App.classes.ProductList.prototype.init = function() {//запись в прототип этого метода
+ 	this.getProducts();
+};
+
+App.classes.ProductList.prototype.getProducts = function() {
+
+	var _this = this; //сохраняем контекст в переменную, потому что в аяксе this будет ссылаться не на instance, а на ajax объект
+
+	$.ajax({
+		url: '/myauction/services/products.json',
+		dataType: 'json',
+		method: 'GET',
+		success: function(data) {
+			_this.render(data);
+		},
+		error: function(jqXHR) {
+			_this.render(jqXHR);
+
+		}
+	});
+};
+
+App.classes.ProductList.prototype.render = function(data) {
+
 
 };
 
-App.classes.ProductList.prototype.init = function() {
-
-}
 
 ;(function() {
-	var elements = $('.product-list');
-	App.instances['product-lists'] = [];
+	var elements = $('.product-list'); //jquery объект, который содержит коллекцию из .product-list. elements массив из элементов с переданным селектором, который возвращается методом $
+	//console.log($('.product-list'));
+	//console.log(document.getElementsByClassName('product-list'));
+	App.instances['product-lists'] = []; //создаем св-во(удобное название), присваиваем ему пустой массив
 
-	for(var i = 0; i < elements.length; i++) {
+	for(var i = 0; i < elements.length; i++) {//elements.length - длинна массива, перебирам массив элементс с помощью цикла, кот. запускаем столько раз, сколь эл-ов лежит в массиве
 		
-		App.instances['product-lists'].push(new App.classes.ProductList(elements[i]));
+		App.instances['product-lists'].push(new App.classes.ProductList(elements[i])); //в созданный массив пушим intance (на каждой иттерации новый). Инстанс мы создается функцией конструткором, вызванной с каждым элеметом массива
 	}
 })();
