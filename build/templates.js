838a55688f9986b885a993f0c119c01c12459b6c
Nikkking <veroandnika@gmail.com>
2016-11-01 17:45:47 +0200

    added jquery method extend

--- build/templates.js
+++ build/templates.js
@@ -1,5 +1,5 @@
-Handlebars.registerPartial("contacts", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div data-components=\"Contacts\"  class=\"contact\">\r\n  <h2 class=\"contact__title\">We need your feedback</h2>\r\n  <div class=\"contact__form\">\r\n  	<form class=\"form\">\r\n		  <div class=\"form__wrapper\">\r\n		    <div class=\"form__col\">\r\n		      <label class=\"form__label\">Name\r\n		        <input class=\"form__input\"  type=\"text\" placeholder=\"How do they call you?...\">\r\n		      </label>\r\n		   		<label class=\"form__label\">Email\r\n		        <input class=\"form__input\"  type=\"text\" placeholder=\"It will be great if you have some shit like that...\">\r\n		      </label>\r\n		   		<label class=\"form__label\">Subject\r\n		        <input class=\"form__input\" type=\"text\" placeholder=\"Better if you have it...\">\r\n		      </label>\r\n		    </div>\r\n		    <div class=\"form__col\">\r\n			    <label class=\"form__label\"> Message\r\n					  <textarea class=\"form__input form__textarea\"  placeholder=\"I need my money back you fucking fucks. Or my race will declare war to yours...\"></textarea>\r\n					</label>\r\n					<div class=\"input-group-button\">\r\n    				<input type=\"submit\" class=\"button\" value=\"Submit\">\r\n  				</div>\r\n		    </div>\r\n		  </div>\r\n		</form>\r\n	</div>\r\n</div>\r\n<div class=\"contact-map\">\r\n	<div class=\"connect\">\r\n		<h3 class=\"connect__title\">Connect with us</h3>\r\n		<div class=\"connect-block\">\r\n			<div class=\"connect__location\">\r\n				<div class=\"contact__location-name\">Gfashion</div>\r\n				<div class=\"contact__location-adress\">Milky way, Solar System, Earth, <br>England, Fuchester, Baker Street 221B</div>\r\n			</div>\r\n			<div class=\"connect__contacts\">\r\n				<div class=\"connect__contacts-item connect__contacts-item_call\">Really nigga?</div>\r\n				<div class=\"connect__contacts-item connect__contacts-item_holo\">GFholo</div>\r\n			</div>\r\n		</div>\r\n	</div>\r\n	<div class=\"contact-map__nav\">\r\n		<div><img src=\"../src/images/map-nav.png\"></div>\r\n		<div><img src=\"../src/images/map-line.png\"></div>\r\n	</div>\r\n</div>";
+Handlebars.registerPartial("category", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"category-block\" data-components=\"Nav\">\r\n	<div class=\"category\">\r\n		<a data-id=\"men\" class=\"category__link\" href=\"#\">\r\n			<img src=\"../src/images/category1.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\"  src=\"../src/images/icon-men.png\">\r\n				<span class=\"category__text-block-title\">Men</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n	<div class=\"category\">\r\n		<a data-id=\"presidents\" class=\"category__link\" href=\"#\">\r\n			<img src=\"../src/images/category2.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\"  src=\"../src/images/icon-president.png\">\r\n				<span class=\"category__text-block-title\">Presidents</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n	<div class=\"category\">\r\n		<a data-id=\"humanoids\" class=\"category__link\" href=\"#\">\r\n			<img src=\"../src/images/category3.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\" src=\"../src/images/icon-humanoids.png\">\r\n				<span class=\"category__text-block-title\">Humanoids</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n	<div class=\"category\">\r\n		<a data-id=\"5d-creatures\" class=\"category__link\" href=\"\">\r\n			<img src=\"../src/images/category4.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\"  src=\"../src/images/5D-icon.png\">\r\n				<span>5D creautures</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n</div>";
 },"useData":true}));
 this["App"] = this["App"] || {};
 this["App"]["templates"] = this["App"]["templates"] || {};
@@ -12,8 +12,8 @@ this["App"]["templates"]["error"] = Handlebars.template({"compiler":[7,">= 4.0.0
     + alias4(((helper = (helper = helpers.statusText || (depth0 != null ? depth0.statusText : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"statusText","hash":{},"data":data}) : helper)))
     + "</div>\r\n</div>";
 },"useData":true});
-Handlebars.registerPartial("category", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"category-block\" data-components=\"Nav\">\r\n	<div class=\"category\">\r\n		<a data-id=\"men\" class=\"category__link\" href=\"#\">\r\n			<img src=\"../src/images/category1.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\"  src=\"../src/images/icon-men.png\">\r\n				<span class=\"category__text-block-title\">Men</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n	<div class=\"category\">\r\n		<a data-id=\"presidents\" class=\"category__link\" href=\"#\">\r\n			<img src=\"../src/images/category2.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\"  src=\"../src/images/icon-president.png\">\r\n				<span class=\"category__text-block-title\">Presidents</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n	<div class=\"category\">\r\n		<a data-id=\"humanoids\" class=\"category__link\" href=\"#\">\r\n			<img src=\"../src/images/category3.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\" src=\"../src/images/icon-humanoids.png\">\r\n				<span class=\"category__text-block-title\">Humanoids</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n	<div class=\"category\">\r\n		<a data-id=\"5d-creatures\" class=\"category__link\" href=\"\">\r\n			<img src=\"../src/images/category4.png\" class=\"category__image\">\r\n			<span class=\"category__text-block\">\r\n				<img class=\"category__text-block-icon\"  src=\"../src/images/5D-icon.png\">\r\n				<span>5D creautures</span>\r\n			</span>\r\n		</a>\r\n	</div>\r\n</div>";
+Handlebars.registerPartial("contacts", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div data-components=\"Contacts\"  class=\"contact\">\r\n  <h2 class=\"contact__title\">We need your feedback</h2>\r\n  <div class=\"contact__form\">\r\n  	<form class=\"form\">\r\n		  <div class=\"form__wrapper\">\r\n		    <div class=\"form__col\">\r\n		      <label class=\"form__label\">Name\r\n		        <input class=\"form__input\"  type=\"text\" placeholder=\"How do they call you?...\">\r\n		      </label>\r\n		   		<label class=\"form__label\">Email\r\n		        <input class=\"form__input\"  type=\"text\" placeholder=\"It will be great if you have some shit like that...\">\r\n		      </label>\r\n		   		<label class=\"form__label\">Subject\r\n		        <input class=\"form__input\" type=\"text\" placeholder=\"Better if you have it...\">\r\n		      </label>\r\n		    </div>\r\n		    <div class=\"form__col\">\r\n			    <label class=\"form__label\"> Message\r\n					  <textarea class=\"form__input form__textarea\"  placeholder=\"I need my money back you fucking fucks. Or my race will declare war to yours...\"></textarea>\r\n					</label>\r\n					<div class=\"input-group-button\">\r\n    				<input type=\"submit\" class=\"button\" value=\"Submit\">\r\n  				</div>\r\n		    </div>\r\n		  </div>\r\n		</form>\r\n	</div>\r\n</div>\r\n<div class=\"contact-map\">\r\n	<div class=\"connect\">\r\n		<h3 class=\"connect__title\">Connect with us</h3>\r\n		<div class=\"connect-block\">\r\n			<div class=\"connect__location\">\r\n				<div class=\"contact__location-name\">Gfashion</div>\r\n				<div class=\"contact__location-adress\">Milky way, Solar System, Earth, <br>England, Fuchester, Baker Street 221B</div>\r\n			</div>\r\n			<div class=\"connect__contacts\">\r\n				<div class=\"connect__contacts-item connect__contacts-item_call\">Really nigga?</div>\r\n				<div class=\"connect__contacts-item connect__contacts-item_holo\">GFholo</div>\r\n			</div>\r\n		</div>\r\n	</div>\r\n	<div class=\"contact-map__nav\">\r\n		<div><img src=\"../src/images/map-nav.png\"></div>\r\n		<div><img src=\"../src/images/map-line.png\"></div>\r\n	</div>\r\n</div>";
 },"useData":true}));
 this["App"]["templates"]["product-list"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
@@ -23,6 +23,11 @@ this["App"]["templates"]["product-list"] = Handlebars.template({"1":function(con
     var stack1;
 
   return ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"usePartial":true,"useData":true});
+this["App"]["templates"]["contacts"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = container.invokePartial(partials.contacts,depth0,{"name":"contacts","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
 },"usePartial":true,"useData":true});
 Handlebars.registerPartial("product", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
@@ -38,14 +43,6 @@ Handlebars.registerPartial("product", Handlebars.template({"compiler":[7,">= 4.0
     + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
     + "</div>\r\n  </div>\r\n</div>";
 },"useData":true}));
-this["App"]["templates"]["contacts"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1;
-
-  return ((stack1 = container.invokePartial(partials.contacts,depth0,{"name":"contacts","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
-},"usePartial":true,"useData":true});
-Handlebars.registerPartial("product-list-component", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div data-components=\"ProductList\" class=\"product-list\">\r\n\r\n</div>";
-},"useData":true}));
 this["App"]["templates"]["index"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
@@ -53,8 +50,8 @@ this["App"]["templates"]["index"] = Handlebars.template({"compiler":[7,">= 4.0.0
     + ((stack1 = container.invokePartial(partials["special-list-component"],depth0,{"name":"special-list-component","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
     + ((stack1 = container.invokePartial(partials.category,depth0,{"name":"category","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
 },"usePartial":true,"useData":true});
-Handlebars.registerPartial("slider", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div data-components=\"mainSlider\" class=\"automatic-slider\">\r\n	<div><img src=\"../src/images/slider-img1.png\"/></div>\r\n	<div><img src=\"../src/images/slider-img2.png\"/></div>\r\n	<div><img src=\"../src/images/slider-img3.png\"/></div>\r\n	<div><img src=\"../src/images/slider-img4.png\"/></div>\r\n</div>\r\n	";
+Handlebars.registerPartial("product-list-component", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div data-components=\"ProductList\" class=\"product-list\">\r\n\r\n</div>";
 },"useData":true}));
 this["App"]["templates"]["plp"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
@@ -62,8 +59,8 @@ this["App"]["templates"]["plp"] = Handlebars.template({"compiler":[7,">= 4.0.0"]
   return ((stack1 = container.invokePartial(partials["product-list-component"],depth0,{"name":"product-list-component","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
     + "\r\n\r\n";
 },"usePartial":true,"useData":true});
-Handlebars.registerPartial("special-list-component", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div data-components=\"SpecialList\" class=\"special-list\"></div>";
+Handlebars.registerPartial("slider", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div data-components=\"mainSlider\" class=\"automatic-slider\">\r\n	<div><img src=\"../src/images/slider-img1.png\"/></div>\r\n	<div><img src=\"../src/images/slider-img2.png\"/></div>\r\n	<div><img src=\"../src/images/slider-img3.png\"/></div>\r\n	<div><img src=\"../src/images/slider-img4.png\"/></div>\r\n</div>\r\n	";
 },"useData":true}));
 this["App"]["templates"]["special-list"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, options, buffer = "";
@@ -81,3 +78,6 @@ this["App"]["templates"]["special-list"] = Handlebars.template({"1":function(con
 
   return ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"usePartial":true,"useData":true});
+Handlebars.registerPartial("special-list-component", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div data-components=\"SpecialList\" class=\"special-list\"></div>";
+},"useData":true}));
--- src/blocks/product-list/product-list.js
+++ src/blocks/product-list/product-list.js
@@ -7,22 +7,12 @@ App.data = {};
 
 App.classes.ProductList = function(element) { //описываем ф-цию конструткор. Элемент каждый раз будет ссылаться на аргумент(переданное занчение при вызове ф-ции), переданный при создании конкретного instance
 	var $root = $(element);//создаем jquery объект и кладем его в переменную, на основе element для каждого instance 
-	//console.log(element);
-	//console.log($root);
-	//this ссылка на instance(объект созданный на основе функции конструткор)
-	// this.data = {}; //this.data это св-во instance в которое мы будем класть данные, полученные с сервера
 	this.elements = { //в этом св-ве мы будем хранить все элементы, с которыми мы будем работать в рамках рута
 		$root: $root,//в св-во объекта this.elements мы записывает значение переменной $root, далее мы сможем обращаться к этой переменной через this.elements.$root
 		$navItem: $('[data-id]'),
 		$window: $(window)
 	};
-	// this.options = {
-	// 	productsInitialCount: 8,
-	// 	productsLoadCount: 4
-	// };
-	// this.pageData = {
-	// 	number: 0
-	// };
+
 	this.handleError = this.handleError.bind(this);
 	this.increasePage = this.increasePage.bind(this);
 	this.checkPositions = _.throttle(this.checkPositions.bind(this), 200);
@@ -31,165 +21,123 @@ App.classes.ProductList = function(element) { //описываем ф-цию к�
 		items: []
 	};
 	this.page = 1;
-
 	this.category = App.instances.Nav[0].category;
-	this.init();//есть цепочка прототипов, и если этот метод не существует в объекте, он берется из прототипа, и так ниже.
-//instance создается в цикле в самовызывающейся ф-ции(описано внизу стр-цы)
+	this.init();
 	this.attachEvents();
 };
 
-App.classes.ProductList.prototype.handleError = function(error) {
+$.extend(App.classes.ProductList.prototype, {
+	handleError: function(error) {
 	var template = App.templates['error'](error);
 
-	this.elements.$root.append(template);
-
+	this.elements.$root.append(template)
 	this.elements.$window.off('.products')
-};
-//метод init используется для того, чтобы вызывать другие методы 
-App.classes.ProductList.prototype.init = function() {//запись в прототип этого метода
-	// if (!App.data.products) {
-	// 	this.getProducts();
-	// } else {
-	// 	this.getCategoryProducts(App.data.products);
-	// }
+	},
 
-	// this.getPageData();
+	init: function() {//запись в прототип этого метода
 
-	this.requestProducts(this.page, this.products)
-		.then(this.increasePage)
-		.then(this.render)
-		.catch(this.handleError);
-};
+		this.requestProducts(this.page, this.products)
+			.then(this.increasePage)
+			.then(this.render)
+			.catch(this.handleError);
+	},
 
-App.classes.ProductList.prototype.increasePage = function(products) {
-	var _this = this;
+	increasePage: function(products) {
+		var _this = this;
 
-	return new Promise(function(resolve, reject) {
-		_this.page++;
-		resolve(products);
-	});
-};
-
-App.classes.ProductList.prototype.requestProducts = function(page, products) {
-	return new Promise(function(resolve, reject) {
-		$.ajax({
-			url: '/Myauction/services/products-' + page + '.json',
-			dataType: 'json',
-			method: 'GET',
-			success: function(data) {
-				for (var i = 0; i < data.items.length; i++) {
-					products.items.push(data.items[i]);
-				}
-
-				resolve(products);
-			},
-			error: function(data) {
-				reject(data);
-			}
+		return new Promise(function(resolve, reject) {
+			_this.page++;
+			resolve(products);
 		});
-	});
-}
+	},
 
-App.classes.ProductList.prototype.getCategory = function(event) {
-	var target = event.target;
-	this.category = $(target).data('id');
-};
+	requestProducts: function(page, products) {
+		return new Promise(function(resolve, reject) {
+			$.ajax({
+				url: '/Myauction/services/products-' + page + '.json',
+				data: {
+					sort: 'ASC',
+					category: 'category',
+					limit: 4
+					
+				},
+				dataType: 'json',
+				method: 'GET',
+				success: function(data) {
+					for (var i = 0; i < data.items.length; i++) {
+						products.items.push(data.items[i]);
+					}
 
-// App.classes.ProductList.prototype.getProducts = function() {
-// 	var _this = this;
+					resolve(products);
+				},
+				error: function(data) {
+					reject(data);
+				}
+			});
+		});
+	},
 
-// 	$.ajax({
-// 		url: '/Myauction/services/products?pages=2',
-// 		dataType: 'json',
-// 		method: 'GET'
-// 	}).then(function(data) {
-// 		App.data.products = data;
-// 		_this.getCategoryProducts(data);
-// 	}).catch(function(jqXHR) {
-// 		_this.render(jqXHR, true);
-// 	});
-// };
+	getCategory: function(event) {
+		var target = event.target;
+		this.category = $(target).data('id');
+	},
 
-App.classes.ProductList.prototype.getCategoryProducts = function(data) {
-	var _this = this;
+    getCategoryProducts: function(data) {
+		var _this = this;
 
-	this.data.items = data.items.filter(function(product) {
-		return product.category.toLowerCase() == _this.category.toLowerCase();
-	});
+		this.data.items = data.items.filter(function(product) {
+			return product.category.toLowerCase() == _this.category.toLowerCase();
+		});
 
-	if (this.data.items.length) {
-		this.render(this.data, false, false, true);
-	} else {
-		this.render(this.data, false, true);
-	}
-};
+		if (this.data.items.length) {
+			this.render(this.data, false, false, true);
+		} else {
+			this.render(this.data, false, true);
+		}
+	},
 
-// App.classes.ProductList.prototype.render = function(data, isError, isEmpty, isInitial) {
-// 	var template;
-// 	if (isError) {//если isError будет true, то будет выполняться это, если нет, то другая часть услоной конструкции
-// 		template = App.templates.error(data);// здесь используется hbs шаблон error
-// 		this.elements.$root.html(template);//html метод jquery объекта. Он вставляет, как html переданный ему параметр
-// 	} else if (isEmpty) {
-// 		this.elements.$root.html('<div>No items</div>');
-// 	} else {
-// 		if (isInitial) {
-// 			var _this = this;
-// 			var initialData = {};
+	render: function(data) {
+		var template = App.templates['product-list'](data);
 
-// 			initialData.items = data.items.filter(function(item, index) {
-// 				return index < _this.options.productsInitialCount;
-// 			});
-// 			template = App.templates['product-list'](initialData);
-			
-// 		} else {
-// 			// template = App.templates['product-list'](this.getCurrentProducts());
-// 		}
-// 		this.elements.$root.html(template);
-// 		this.getPageData();
-// 	}
-// };
+		this.elements.$root.html(template);
+	},
 
-App.classes.ProductList.prototype.render = function(data) {
-	var template = App.templates['product-list'](data);
+	checkPositions: function() {
+		var scrollPosition = this.elements.$window.scrollTop() + this.elements.$window.height();
 
-	this.elements.$root.html(template);
-}
-
-App.classes.ProductList.prototype.checkPositions = function() {
-	var scrollPosition = this.elements.$window.scrollTop() + this.elements.$window.height();
-
-	if (scrollPosition >= this.elements.$root.offset().top + this.elements.$root.height()) {
+		if (scrollPosition >= this.elements.$root.offset().top + this.elements.$root.height()) {
 			this.requestProducts(this.page, this.products)
 				.then(this.increasePage)
 				.then(this.render)
 				.catch(this.handleError);
 		}
-};
+	},
 
-App.classes.ProductList.prototype.getPageData = function() {
-	this.pageData.height = this.elements.$root.height();
-	this.pageData.bottom = this.pageData.height + this.elements.$root.offset().top;
-};
+	getPageData: function() {
+		this.pageData.height = this.elements.$root.height();
+		this.pageData.bottom = this.pageData.height + this.elements.$root.offset().top;
+	},
 
-App.classes.ProductList.prototype.getCurrentProducts = function() {
-	var _this = this;
-	var data = {};
+	getCurrentProducts: function() {
+		var _this = this;
+		var data = {};
 
-	this.pageData.number++;
-	
-	var startIndex = this.options.productsInitialCount + this.options.productsLoadCount * (this.pageData.number - 1);
-	var endIndex = startIndex + this.options.productsLoadCount;
+		this.pageData.number++;
+		
+		var startIndex = this.options.productsInitialCount + this.options.productsLoadCount * (this.pageData.number - 1);
+		var endIndex = startIndex + this.options.productsLoadCount;
 
-	data.items = this.data.items.filter(function(item, index) {
-		return (index >= startIndex && index < endIndex);
-	});
+		data.items = this.data.items.filter(function(item, index) {
+			return (index >= startIndex && index < endIndex);
+		});
 
-	return data;
-};
+		return data;
+	},
 
-App.classes.ProductList.prototype.attachEvents = function() {
-	this.elements.$window.on('scroll.products', this.checkPositions);
-};
+	attachEvents: function() {
+		this.elements.$window.on('scroll.products', this.checkPositions);
+	}
+});
 
 
 
