da80e1cf4bf75d964471415e27a6570d8a4452ff
Nikkking <veroandnika@gmail.com>
2016-12-05 19:50:55 +0200

    added styles for slider

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,27 +1,26 @@
-var App = App || {};//создаем глобальную переменную. Пространство имен приложения.
+var App = App || {};
 
-App.instances = App.instances || {};//создаем св-во объекта, будет хранить все инстансы. Инстансы - это объекты созданные при вызове ф-ции кнструктора со словом new
-
+App.instances = App.instances || {};
 App.classes = App.classes || {};
 
-App.classes.mainSlider = function(element) { //описываем ф-цию конструткор. Элемент каждый раз будет ссылаться на аргумент(переданное занчение при вызове ф-ции), переданный при создании конкретного instance
-	var $root = $(element);//создаем jquery объект и кладем его в переменную, на основе element для каждого instance 
-	//console.log(element);
-	//console.log($root);
-	//this ссылка на instance(объект созданный на основе функции конструткор)
-	this.data = null; //this.data это св-во instance в которое мы будем класть данные, полученные с сервера
-	this.elements = { //в этом св-ве мы будем хранить все элементы, с которыми мы будем работать в рамках рута
-		$root: $root //в св-во объекта this.elements мы записывает значение переменной $root, далее мы сможем обращаться к этой переменной через this.elements.$root
+App.classes.mainSlider = function(element) { 
+	var $root = $(element);
+	this.data = null; 
+	this.elements = { 
+		$root: $root 
 	};
-	//console.log(this.elements.$root);
-	this.init();//есть цепочка прототипов, и если этот метод не существует в объекте, он берется из прототипа, и так ниже.
-//instance создается в цикле в самовызывающейся ф-ции(описано внизу стр-цы)
+	this.init();
 };
-//метод init используется для того, чтобы вызывать другие методы 
-App.classes.mainSlider.prototype.init = function() {//запись в прототип этого метода
+
+App.classes.mainSlider.prototype.init = function() {
 	this.slider();
-}
+};
 
 App.classes.mainSlider.prototype.slider = function() {
-	this.elements.$root.slick();
+	this.elements.$root.slick({
+		autoplay: true,
+  		autoplaySpeed: 2500,
+  		fade: true,
+  		cssEase: 'linear'
+	});
 };
