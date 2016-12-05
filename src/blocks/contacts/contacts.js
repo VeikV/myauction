09ead066ce80f152e0a83eea49244c39eb10a80b
Nikkking <veroandnika@gmail.com>
2016-12-05 20:19:16 +0200

    added changes to contacts

--- src/blocks/contacts/contacts.js
+++ src/blocks/contacts/contacts.js
@@ -1,24 +1,20 @@
-var App = App || {};//создаем глобальную переменную. Пространство имен приложения.
+var App = App || {};
 
-App.instances = App.instances || {};//создаем св-во объекта, будет хранить все инстансы. Инстансы - это объекты созданные при вызове ф-ции кнструктора со словом new
+App.instances = App.instances || {};
 
-App.classes = App.classes || {};//создаем второе св-во объекта. Будет хранить все классы(функции конструткоры и их прототипы), которые создают instances
+App.classes = App.classes || {};
 
-
-App.classes.Contacts = function(element) { //описываем ф-цию конструткор. Элемент каждый раз будет ссылаться на аргумент(переданное занчение при вызове ф-ции), переданный при создании конкретного instance
-	var $root = $(element);//создаем jquery объект и кладем его в переменную, на основе element для каждого instance 
-	//console.log(element);
-	//console.log($root);
-	//this ссылка на instance(объект созданный на основе функции конструткор)
-	this.data = null; //this.data это св-во instance в которое мы будем класть данные, полученные с сервера
-	this.elements = { //в этом св-ве мы будем хранить все элементы, с которыми мы будем работать в рамках рута
-		$root: $root //в св-во объекта this.elements мы записывает значение переменной $root, далее мы сможем обращаться к этой переменной через this.elements.$root
+App.classes.Contacts = function(element) { 
+	var $root = $(element);
+	
+	this.data = null; 
+	this.elements = {
+		$root: $root
 	};
-	//console.log(this.elements.$root);
-	this.init();//есть цепочка прототипов, и если этот метод не существует в объекте, он берется из прототипа, и так ниже.
-//instance создается в цикле в самовызывающейся ф-ции(описано внизу стр-цы)
-};
-//метод init используется для того, чтобы вызывать другие методы 
-App.classes.Contacts.prototype.init = function() {//запись в прототип этого метода
 
-}
+	this.init();
+};
+
+App.classes.Contacts.prototype.init = function() {
+
+};
